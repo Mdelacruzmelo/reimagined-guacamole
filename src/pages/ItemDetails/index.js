b762b8faf3041a301edefa90b3615e67b3b73291
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-20 02:36:29 +0200

    Implemented page not found style

--- src/pages/ItemDetails/index.js
+++ src/pages/ItemDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import { useParams, useNavigate } from "react-router-dom";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 // import FadeIn from 'react-fade-in';
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const NotFound = () => {
 
     const navigate = useNavigate()
 
     return (
-        <div>
-            Product not found
-            <button onClick={() => { navigate("/") }}>Go to list</button>
+        <div className='page'>
+            <div className='not_found'>
+                <div className='title'>
+                    Product not found
+                </div>
+                <button onClick={() => { navigate("/") }}>
+                    <AiOutlineArrowLeft />  Go back to list
+                </button>
+            </div>
         </div>
     );
 }
